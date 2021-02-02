cbb6ac0307a975e7506c85ec0d65ebf3ffc801fe
Pratik Jagade <pratik.21810767@viit.ac.in>
2021-02-02 23:11:25 +0530

    Directory structore setup for client side

--- src/transaction.ts
+++
@@ -1,6 +0,0 @@
-import { transactionStructure } from "./transactionAbstract";
-
-export const transactions:transactionStructure[]=[
-  {type: "income",text: "salary", amount: 1000},
-  {type: "expense",text: "camera", amount: 1000}
-];
